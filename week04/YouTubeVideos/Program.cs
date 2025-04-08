992ef6e28e5bdae3c6bdfb3c6e60aaded82ac342
Brandon Chimuti <beechimuti@gmail.com>
2025-04-08 06:15:49 +0200

    made code

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,85 @@
 using System;
+using System;
+using System.Collections.Generic;
+
+class Comment
+{
+    public string Name { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string name, string text)
+    {
+        Name = name;
+        Text = text;
+    }
+}
+
+class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; } // in seconds
+    private List<Comment> Comments { get; set; }
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        Comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return Comments.Count;
+    }
+
+    public void DisplayVideoDetails()
+    {
+        Console.WriteLine($"Title: {Title}\nAuthor: {Author}\nLength: {Length} seconds");
+        Console.WriteLine($"Number of Comments: {GetCommentCount()}");
+        Console.WriteLine("Comments:");
+        foreach (var comment in Comments)
+        {
+            Console.WriteLine($"- {comment.Name}: {comment.Text}");
+        }
+        Console.WriteLine(new string('-', 40));
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("C# Basics", "John Doe", 600);
+        video1.AddComment(new Comment("Alice", "Great tutorial!"));
+        video1.AddComment(new Comment("Bob", "Very helpful, thanks!"));
+        video1.AddComment(new Comment("Charlie", "Awesome explanation!"));
+
+        Video video2 = new Video("OOP in C#", "Jane Smith", 1200);
+        video2.AddComment(new Comment("David", "Really clear examples."));
+        video2.AddComment(new Comment("Eve", "Helped me understand abstraction."));
+        video2.AddComment(new Comment("Frank", "Looking forward to more!"));
+
+        Video video3 = new Video("Advanced C#", "Alice Johnson", 1800);
+        video3.AddComment(new Comment("Grace", "Loved the advanced topics!"));
+        video3.AddComment(new Comment("Hank", "Well explained."));
+        video3.AddComment(new Comment("Ivy", "Fantastic breakdown of concepts."));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        foreach (var video in videos)
+        {
+            video.DisplayVideoDetails();
+        }
     }
 }
